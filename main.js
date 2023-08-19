91236cf34c49576ccc4122b4852d2ccfe0fbd863
Yansilv55 <yansilv55@gmail.com>
2023-08-19 12:11:21 -0300

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//String
+
+const firstName = 'Yan'
+const lastName  = 'Silva'
+
+const names = 'Yan, Lívia, Joel'
+
+console.log(`Meu nome é ${firstName} ${lastName}`)
+console.log(`Meu nome é ${firstName.toUpperCase} ${lastName.toLowerCase}`)
+console.log(names.split(","))
+
+//Number
+const number = 10;
+console.log(number / 2)
+
+//Booleanos
+false 
+true
+
+console.log(2 == 3)
+
+//Null & Underfined
+
+const x = null;
+
+const y = undefined;
