624ba4e6bd3581f9bb53868785d04e29c960e25c
Yansilv55 <yansilv55@gmail.com>
2023-08-25 11:10:57 -0300

    commit

---
+++ aula20/main.js
@@ -0,0 +1,15 @@
+/*var c = 1;
+
+while(c <= 10) {
+    console.log(c)
+    c++;
+}
+*/
+
+//Fazer a mesma coisa com o for
+
+console.log('Vai começar...')
+for(var c = 1;c <= 15;c++) {
+    console.log(c)
+}
+console.log('FIM!')
